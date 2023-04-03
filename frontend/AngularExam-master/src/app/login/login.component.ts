6e00e2a801a0aaad9645ce8a406bab2f57be844f
Riccardo Gavioli <riccardo.gavioli@pubcoder.com>
2023-04-03 17:19:21 +0200

    now we create a blob into profile of user but I can't read it for the fucking problem of this stupid and idiot browser

--- frontend/AngularExam-master/src/app/login/login.component.ts
+++ frontend/AngularExam-master/src/app/login/login.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 import {AppStateService} from "../app-state.service";
 import {NgToastService} from "ng-angular-popup";
 import {HttpClient} from "@angular/common/http";
-import {UserInfoI} from "../data/utenti";
+
+
 
 @Component({
   selector: 'app-login',
@@ -15,54 +16,23 @@ export class LoginComponent {
   password: string = "";
 
   show: boolean = false;
+  selectedFile: File = {} as File
+
+  fileURL: string = "";
 
 
   constructor(public appServ: AppStateService, private toast: NgToastService, private http: HttpClient) {
   }
 
-  // submit(username: string, password: string) {
-  //
-  //   this.http.get<UserInfoI[]>('http://localhost:8080/api/v1/users/login?mail='+ username +'&password='+password).subscribe({
-  //     next: data => {
-  //       this.toast.success({detail:'Success',summary:"Login effettuato", duration: 3000});
-  //       console.warn(data);
-  //       let window = document.getElementById('id01');
-  //         if (window != null) [
-  //           window.style.display = 'none',
-  //         ]
-  //     },
-  //     error: error => {
-  //       this.toast.error({detail:'Error',summary:"Username o Password non corretti", duration: 3000});
-  //       console.error('There was an error!', error);
-  //     }
-  //   })
-
-
-  // if (this.username === this.password) {
-  //   localStorage.setItem('loggedUser', this.username);
-  //   this.toast.success({detail:'Success',summary:"Login effettuato", duration: 3000});
-  //   let window = document.getElementById('id01');
-  //   if (window != null) [
-  //     window.style.display = 'none',
-  //   ]
-  // }else {
-  //   this.toast.error({detail:'Error',summary:"Username o Password non corretti", duration: 3000});
-  //   return
-  // }
-//     this.clear();
-//   }
-//   clear(){
-//     this.username ="";
-//     this.password = "";
-//   }
-
 
   onSubmit(contactForm) {
     console.log(contactForm.value);
     let product = contactForm.value;
+    product.image = this.fileURL;
     console.log(product);
-    const url = 'http://localhost:8083/api/v1/users/create';
 
+
+    const url = 'http://localhost:8083/api/v1/users/create';
     this.http.post(url, product).subscribe(response => {
       console.log(response); // Risposta del server
       this.toast.success({
@@ -76,7 +46,13 @@ export class LoginComponent {
       ]
       console.warn(contactForm);
       contactForm.reset();
-
     });
+  }
+
+
+  onFileSelected(event) {
+    this.selectedFile = <File>event.target.files[0];
+    this.fileURL = URL.createObjectURL(this.selectedFile);
+    console.warn(this.fileURL)
   }
 }
